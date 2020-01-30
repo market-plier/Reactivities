7b6763d14c793b07c8b58a0408f48bf86ecea141
gl0bo_0 <maksimenkogleb@gmail.com>
2020-01-30 22:17:23 +0200

    StylesNotWorking

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import {List} from 'semantic-ui-react';
+import axios from 'axios';
+import { IActivity } from '../models/activity';
+import { NavBar } from '../../features/nav/NavBar';
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([])
+
+  useEffect (()=> {
+    
+    axios.get<IActivity[]>('http://localhost:5000/api/activities')
+    .then((response)=>{
+      setActivities(response.data)
+    });
+  }, []);
+ 
+    return (
+      <div>
+         <NavBar />
+  <List>
+  <ul>
+           {activities.map((activity)=> (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+           ))}
+         </ul>
+    
+  </List>
+       
+        
+      </div>
+    );
+  
+  
+}
+
+export default App;
